604f63a6f76efd2c2a4bb7c9ba13e53380e89167
crypto.schmoi <tayla.akurangi@gmail.com>
2023-09-12 14:22:40 +1000

    Updated function so row and column are merged

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,20 +6,18 @@ const columns = 16; // Creates a variable for number of columns (16)
 
 const grid = document.querySelector('.grid'); // Selects the div class from the HTML 
 
-function createRows() {
-    for (let i = 0; i < 'rows'; i++) { // Creates rows using the number of rows from variable
+function createGrid() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) { // Creates rows using the number of rows from variable
         const cell = document.createElement('div'); // Adds div elements
+        cell.classList.add('cell');
         grid.appendChild(cell);
+     }
     }
+    console.log("Grid created successfully.");
 }
 
-function createColumns() {
-    for (let j = 0; j < 'columns'; j++) { // Creates columns using the number of rows from variable
-        const cell = document.createElement('div'); // Adds div elements 
-        grid.appendChild(cell);
-    }
-}
-
+createGrid();
 // It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
 
 // You need to make the divs appear as a grid (versus just one on each line). This is a perfect opportunity to apply what you have learned about flexbox.
