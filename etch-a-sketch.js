1501f30109acb092f626bf8d86209afcb5e7e711
crypto.schmoi <tayla.akurangi@gmail.com>
2023-09-13 12:18:25 +1000

    Added random colour selection for the grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,5 @@
 // Created a webpage with a 16x16 grid of square divs.
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM is ready.");
 
 const rows = 16; // Creates a variable for number of rows (16)
 const columns = 16; // Creates a variable for number of columns (16)
@@ -9,28 +8,37 @@ const grid = document.querySelector('.grid'); // Selects the div class "grid" fr
 
 createGrid = () => {
     for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            
-            // Add the mouseenter event listener here
-            cell.addEventListener("mouseenter", function(event) {
-                event.target.style.backgroundColor = "purple";
-            });
-            
-            // Append the cell to the grid once
-            grid.appendChild(cell);
+    for (let j = 0; j < columns; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+
+    // Changes the cell to a random colour when mouse hovers over
+    function randomColor() {
+        let color = [];
+        for (let i = 0; i < 3; i++) {
+            color.push(Math.floor(Math.random() * 256));
         }
+        return 'rgb(' + color.join(', ') + ')';
+        } 
+    
+    // // Added the mouseenter event listener
+    cell.addEventListener("mouseenter", function(event) {
+        event.target.style.backgroundColor = randomColor();
+    });
+    
+    // Appends the cell to the grid once
+    grid.appendChild(cell);
+    }
     }
 }
-    console.log("grid is ready.");
 
-    function removeAllChildNodes(parent){
-        while(parent.firstChild){
-            parent.removeChild(parent.firstChild);
-        }
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
+}
 
+// Code for the slider to change the size of the cell and background colour whem mouse hovers over
 const slider = document.querySelector ('.slider'); // Selects the input class "slider" from the HTML
 
 slider.addEventListener('input', function(){
@@ -41,8 +49,18 @@ slider.addEventListener('input', function(){
     for (let i = 0; i < val*val; i++) {
         const div = document.createElement('div');
         div.classList.add('cell');
+
+        // Function created so that the random colour of the cell to be selected when cell size changes
+        function randomColor() {
+            let color = [];
+            for (let i = 0; i < 3; i++) {
+              color.push(Math.floor(Math.random() * 256));
+            }
+            return 'rgb(' + color.join(', ') + ')';
+          } 
+
         div.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = 'black';
+            event.target.style.backgroundColor = randomColor();
         })
         grid.appendChild(div); 
     }
@@ -51,3 +69,5 @@ slider.addEventListener('input', function(){
 createGrid();
 
  });
+
+ 
